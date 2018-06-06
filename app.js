72e99a6e02012cd7e0a113dc26cfe80182e3ccd3
icyhearts <icyhearts@163.com>
2018-06-06 17:26:12 +0800

    add bat

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+function getStr()
+{
+  str = "<h1>you know? </h1>\
+  <p>chinese nation is good</p>"
+  return str;
 
+}
 app.get('/', function (req, res) {
-  res.send('Hello World!!!!')
+//  res.send('Hello World!!!!')
+  res.send(getStr() );
 })
 
 app.listen(3000, function () {
